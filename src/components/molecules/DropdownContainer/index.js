3648b510372b34bccb89b48bcf5e9d7ee8f9842c
Kriti Jain <jain.kriti@zomato.com>
2022-11-06 20:21:04 +0530

    Open Jobs list

---
+++ src/components/molecules/DropdownContainer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DropdownContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DropdownContainer
---
+++ src/components/organisms/OpenJobsList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import mockList from '../mockdata.js'
+const OpenJobsList = () => {
+    
+    const jobList = [...mockList];
+    console.log()
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OpenJobsList
